5c16a9687018d685b0caa08fe65da2404ef52295
hani-sabaie <sabaieh@tbzmed.ac.ir>
2025-12-26 13:08:18 +0000

    Deploying to gh-pages from @ hani-sabaie/hani-sabaie.github.io@cdd03855da9d3e98bac29e75463fc27a66de3ad6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -411,7 +411,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-a-bidirectional-mendelian-randomization-study-on-causal-biomarkers-of-multiple-sclerosis-progression",
           title: 'A Bidirectional Mendelian Randomization Study on Causal Biomarkers of Multiple Sclerosis Progression',
-          description: "Published Mendelian Randomization analyses identifying causal biomarkers of MS progression",
+          description: "Genetic dissection of mitochondrial DNA copy number and leukocyte telomere length in multiple sclerosis progression",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
